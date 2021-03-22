4acf96465c46ff141b5eb6fb23da214d7e859a30
Mati <rehman_m31@yahoo.com>
2021-03-22 14:35:06 +0500

    Getting the directories in shape

---
+++ admin/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return(
+    <div>
+      <h1>This is the root component of our admin site. Start righting your code here.</h1>
+    </div>
+  );
+}
+
+export default App;
