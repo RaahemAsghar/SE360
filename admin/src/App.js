b427f7eb01f7cedaf63945e20340962d60824abb
raahem <raahemasghar97@gmail.com>
2021-04-07 02:28:13 +0500

    admin login frontend

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Homepage from './Homepage.js'
 import Sidebar from './Sidebar.js'
+import Login from './Login.js'
 
 function App() {
   return(
     <div>
-      <Homepage/>
-      <Sidebar/>
+      <Login/>
 
     </div>
   );
