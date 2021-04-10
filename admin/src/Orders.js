a3492b2b5dfbab6f9daba10aca460c1e0d789dfd
hamzashahzad1 <22100027@lums.edu.pk>
2021-04-11 03:03:21 +0500

    Update to orders Screen

--- admin/src/Orders.js
+++ admin/src/Orders.js
@@ -7,7 +7,9 @@ function Orders ({router,updateOrder}) {
     let [details, updateDetails] = React.useState([]);
     // let products = {4:2}
 
-    const handleClick = () => {
+    const handleClick = (event) => {
+        let text = event.target.innerText
+        console.log(text);
         let db = fireApp.database();
         db.ref("products").once('value').then((snap) => {
             let obj = snap.val();
@@ -36,9 +38,10 @@ function Orders ({router,updateOrder}) {
         <div style = {{marginLeft: "90px"}}>
             <h3 style = {{marginTop: "25px", fontFamily: "Arial", fontWeight: "Bold"}}>Pending Orders</h3>
             
-            <button onClick = {handleClick} style = {{marginTop: "30px", backgroundColor:"#DBDFF0", width:"820px", height:"80px",border:"none", textAlign: "left", textIndent: "10px", fontFamily: "Arial", borderRadius: "15px"}}><p style = {{marginTop: "15px", fontWeight: "bold"}}>Order Number: 435</p><p style = {{marginTop: "-10px"}}>Email ID: 22100027@lums.edu.pk</p></button>
+            <button style = {{marginTop: "30px", backgroundColor:"#DBDFF0", width:"820px", height:"80px",border:"none", textAlign: "left", textIndent: "10px", fontFamily: "Arial", borderRadius: "15px", cursor:"context-menu"}}><p style = {{marginTop: "15px", fontWeight: "bold"}}>Order Number: <span onClick = {handleClick} style = {{cursor: "pointer", color: "#0000EE", textDecoration: "underline"}}>435</span></p><p style = {{marginTop: "-10px",cursor: "context-menu"}}>Email ID: 22100027@lums.edu.pk</p>
+            </button>
 
-            <button style = {{marginTop: "20px", backgroundColor:"#DBDFF0", width:"820px", height:"80px",border:"none", textAlign: "left", textIndent: "10px", fontFamily: "Arial", borderRadius: "15px"}}><p style = {{marginTop: "15px", fontWeight: "bold"}}>Order Number: 435</p><p style = {{marginTop: "-10px"}}>Email ID: 22100027@lums.edu.pk</p></button>
+            <button style = {{marginTop: "20px", backgroundColor:"#DBDFF0", width:"820px", height:"80px",border:"none", textAlign: "left", textIndent: "10px", fontFamily: "Arial", borderRadius: "15px"}}><p style = {{marginTop: "15px", fontWeight: "bold"}}>Order Number: 435</p><p style = {{marginTop: "-10px", cursor: "context-menu"}}>Email ID: 22100027@lums.edu.pk</p></button>
         </div>
     )
 }
@@ -64,54 +67,14 @@ function SingleOrder ({details}) {
         return table;
     }
     React.useEffect(() => {
-        let db = fireApp.database();
-        // console.log(details[3]);
-        /*
-        db.ref("products").once('value').then((snap) => {
-            let obj = snap.val();
-            let keys = Object.keys(obj); let values = Object.values(obj);
-            let myDict = {}
-            for(let i = 0; i< keys.length; i++){
-                myDict[keys[i]] = values[i]
-            }
-            let itemKeys = Object.keys(details[3]);
-            // console.log("Hello")
-            let majList = []
-            for(let i = 0; i<itemKeys.length; i++){
-                let minList = []; let price = myDict[itemKeys[i]]["price"];
-                minList.push(myDict[itemKeys[i]]["name"]);
-                minList.push(itemKeys[i]);
-                minList.push(details[3][itemKeys[i]]);
-                minList.push((price*minList[2]));
-                majList.push(minList);
-            }
-            // console.log(majList);
-            // console.log(orderList);
-            updateOrderList(majList);
-            // console.log(orderList.length);
-            // let outList = []
-            // console.log(majList)
-            // for(let i = 0; i<majList.length; i++){
-            //     let minList = [];
-            //     // console.log(orderList[0]);
-            //     for(let j = 0; j<4; i++){
-            //         minList.push(<td style = {{border: "1px solid #828282"}}>{(majList[i][j])}</td>)
-            //     }
-            //     outList.push(<tr style = {{height: "40px"}}>{minList}</tr>);
-            // }
-            // setTable(outList);
-            // updateProductList(myDict);
-        }) */
         let greyHeight = details[4];
-        console.log(greyHeight);
-        // greyHeight = greyHeight + 150;
         greyHeight = greyHeight + "px";
         document.getElementById("greyArea").style.height = greyHeight;
     },[])
     return(
         <div style = {{marginLeft: "90px"}}>
             <div style = {{marginTop: "40px"}}></div>
-            <div id = "greyArea" style = {{backgroundColor: "#DBDFF0", borderRadius: "15px"}}>
+            <div id = "greyArea" style = {{backgroundColor: "#DBDFF0", borderRadius: "15px", width: "860px"}}>
                 <div style = {{fontFamily: "Arial", textIndent: "15px"}}>
                     <p style = {{position: "relative", fontWeight: "bold", top: "20px"}}>Order number: {details[1]}</p>
                     <p style = {{marginTop: "25px"}}>Email Id: {details[2]}</p>
